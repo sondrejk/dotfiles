39f329f3c6f4851c0f63edecf290ef9d21822f02
sondrejk <sondrejk@gmail.com>
2025-09-02 21:19:24 +0200

    Change symlink and add wezterm

--- symlink.sh
+++ symlink.sh
@@ -1,7 +1,13 @@
+rm ~/.tmux.conf
+rm ~/.vimrc
+rm ~/.zshrc
+rm ~/.config/nvim
+rm ~/.config/fastfetch
+rm ~/.config/wezterm
 
-ln -s $(pwd)/.tmux.conf ~/.tmux.conf
-ln -s $(pwd)/.vimrc ~/.vimrc
-ln -s $(pwd)/.zshrc ~/.zshrc
-ln -s $(pwd)/nvim ~/.config/
-ln -s $(pwd)/fastfetch ~/.config/fastfetch
-
+ln -s "$(pwd)"/.tmux.conf ~/.tmux.conf
+ln -s "$(pwd)"/.vimrc ~/.vimrc
+ln -s "$(pwd)"/.zshrc ~/.zshrc
+ln -s "$(pwd)"/nvim ~/.config/
+ln -s "$(pwd)"/fastfetch ~/.config/
+ln -s "$(pwd)"/wezterm ~/.config/
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,34 +1,35 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
+local config = {}
 
+-- Use the config builder
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
--- This table will hold the configuration.
-local config = {}
-config.wsl_domains = {
-  {
-    -- The name of this specific domain.  Must be unique amonst all types
-    -- of domain in the configuration file.
-    name = 'WSL:Ubuntu',
+-- Check if windows
+if wezterm.target_triple:find("windows") ~= nil then
+	config.wsl_domains = {
+		{
+			-- The name of this specific domain.  Must be unique amongst all types
+			-- of domain in the configuration file.
+			name = "WSL:Ubuntu",
 
-    -- The name of the distribution.  This identifies the WSL distribution.
-    -- It must match a valid distribution from your `wsl -l -v` output in
-    -- order for the domain to be useful.
-    distribution = 'Ubuntu',
-    default_cwd = "/home/sondrejk/"
-  },
-}
-config.default_domain = 'WSL:Ubuntu'
+			-- The name of the distribution.  This identifies the WSL distribution.
+			-- It must match a valid distribution from your `wsl -l -v` output in
+			-- order for the domain to be useful.
+			distribution = "Ubuntu",
+			default_cwd = "/home/sondrejk/", -- Sets default directory
+		},
+	}
+	config.default_domain = "WSL:Ubuntu"
+end
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
+-- Font
+config.font = wezterm.font("JetBrains Mono")
 
-config.font = wezterm.font 'JetBrains Mono'
+-- Color scheme
+config.color_scheme = "Gruvbox Dark (Gogh)"
 
--- For example, changing the color scheme:
-config.color_scheme = 'Gruvbox Dark (Gogh)'
-
--- and finally, return the configuration to wezterm
 return config
+
