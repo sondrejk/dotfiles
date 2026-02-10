78ddc5924cef7baf5eb73d0bd84f6f9015f96179
sondrejk <sondrejk@gmail.com>
2026-02-10 13:08:49 +0100

    keymap to disable copilot

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -10,3 +10,12 @@ vim.keymap.set("n", "<C-Space>", "<Cmd>NvimTmuxNavigateNavigateNext<CR>", { sile
 
 vim.g.copilot_no_tab_map = true
 vim.keymap.set("i", "<S-Tab>", 'copilot#Accept("\\<S-Tab>")', { expr = true, replace_keycodes = false })
+
+vim.keymap.set("n", "<leader>ai", function()
+  local copilot = require("copilot.suggestion")
+  if copilot.is_visible() then
+    copilot.dismiss()
+  else
+    -- Assuming you have another way to enable it or just want to toggle
+  end
+end, { desc = "Toggle Copilot Suggestion" })
