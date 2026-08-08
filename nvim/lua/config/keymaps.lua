6c0a4da439db55d9d962a145edaaeaee81f25b18
sondrejk <sondrejk@gmail.com>
2026-08-08 22:49:14 +0200

    fix: vim tmux navigation

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -6,7 +6,7 @@ vim.keymap.set("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>", { silent = true })
 vim.keymap.set("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>", { silent = true })
 vim.keymap.set("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", { silent = true })
 vim.keymap.set("n", "<C-\\>", "<Cmd>NvimTmuxNavigateLastActive<CR>", { silent = true })
-vim.keymap.set("n", "<C-Space>", "<Cmd>NvimTmuxNavigateNavigateNext<CR>", { silent = true })
+vim.keymap.set("n", "<C-Space>", "<Cmd>NvimTmuxNavigateNext<CR>", { silent = true })
 
 vim.g.copilot_no_tab_map = true
 vim.keymap.set("i", "<S-Tab>", 'copilot#Accept("\\<S-Tab>")', { expr = true, replace_keycodes = false })
--- nvim/lua/plugins/nvim-tmux-navigation.lua
+++ nvim/lua/plugins/nvim-tmux-navigation.lua
@@ -2,16 +2,9 @@ return {
   {
     "alexghergh/nvim-tmux-navigation",
     config = function()
+      -- keybindings live in lua/config/keymaps.lua
       require("nvim-tmux-navigation").setup({
         disable_when_zoomed = true, -- defaults to false
-        keybindings = {
-          left = "<C-h>",
-          down = "<C-j>",
-          up = "<C-k>",
-          right = "<C-l>",
-          last_active = "<C-\\>",
-          next = "<C-Space>",
-        },
       })
     end,
   },
