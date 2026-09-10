5fc94250e10bf4866e1db1a2b716f578a4c5dc36
sondrejk <sondrejk@gmail.com>
2026-09-10 18:47:22 +0200

    feat: typst and latex writing

---
+++ nvim/lua/plugins/typst-preview.lua
@@ -0,0 +1,5 @@
+-- Browser-based preview is glitchy on zoom/scroll; using zathura + tmux
+-- instead, bound to the same <leader>cp key (see config/autocmds.lua).
+return {
+  { "chomosuke/typst-preview.nvim", enabled = false },
+}
